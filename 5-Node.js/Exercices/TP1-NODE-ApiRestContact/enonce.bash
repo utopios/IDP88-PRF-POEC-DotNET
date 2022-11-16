662a1ada1b9cf1e8e0a4fd5d7e2f303b228d9e7d
utopios <github@utopios.net>
2022-11-16 13:53:56 +0100

    add TP

---
+++ 5-Node.js/Exercices/TP1-NODE-ApiRestContact/enonce.bash
@@ -0,0 +1,7 @@
+# Sur lmodel de l'API CoursList créer une API Orienté Objet avec
+# 1- Une Classe Person
+    # - Attibuts : id, title, firstname, lastname, dateOfBirth, created , updated 
+# 2- Une class Contact hérite de Peron
+    # - Attibuts : Phone, Email
+
+# 3- Rendre l'upload d'un image possible ainsi que son accès static pour une utilisation futur par un projet Front React
