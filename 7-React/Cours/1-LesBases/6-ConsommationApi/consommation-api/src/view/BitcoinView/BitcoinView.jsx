fd5d0af7172a371f3319132f820b595c772f719d
utopios <github@utopios.net>
2022-11-25 16:14:24 +0100

    add TP-1 ApiSirene

--- 7-React/Cours/1-LesBases/6-ConsommationApi/consommation-api/src/view/BitcoinView/BitcoinView.jsx
+++ 7-React/Cours/1-LesBases/6-ConsommationApi/consommation-api/src/view/BitcoinView/BitcoinView.jsx
@@ -4,6 +4,8 @@ const BitcoinView = () => {
 
     const [loading, setloading] = useState(true);
     const [data, setData] = useState(null);
+    const [currency, setCurrency] = useState(null);
+    const [priceData, setPriceData] = useState(null);
 
     useEffect(() => {
         async function fetchPrices() {
@@ -11,6 +13,8 @@ const BitcoinView = () => {
             const resJson = await res.json();
             if (resJson !== null) {
                 setData(resJson);
+                setCurrency(resJson.bpi.EUR.code);
+                setPriceData(resJson.bpi);
                 setloading(false);
                 console.log(resJson.bpi);
             }
@@ -26,15 +30,29 @@ const BitcoinView = () => {
                         <img src="./img/loading.svg" alt="loading-logo" />
                     </div>
                     :
-                    <div className='priceContainer'>
-                        <h1>{data.chartName}</h1>
-                        <div>{data.disclaimer}</div>
-                        <div>{data.time.updated}</div>
-                        <div>{data.bpi.EUR.code} : {data.bpi.EUR.rate_float} €</div>
-                        <div></div>
+                    // <div className='priceContainer'>
+                    //     <h1>{data.chartName}</h1>
+                    //     <div>{data.disclaimer}</div>
+                    //     <div>{data.time.updated}</div>
+                    //     <div>{data.bpi.EUR.code} : {data.bpi.EUR.rate_float} €</div>
+                    // </div>
+
+                    <div>
+                        <select name="currency" id="currency" onChange={(e) => setCurrency(e.target.value)}>
+                            <option value="EUR">EUR (€)</option>
+                            <option value="USD">USD ($)</option>
+                            <option value="GBP">GBP (£)</option>
+                        </select>
+
+                        <div className='bitcoinPrice'>
+                            <span>
+                                <b>{currency}</b> - Price : <b>{Math.round(priceData[currency].rate_float * 100) / 100}</b> {currency === "EUR" ? "€" : currency === "USD" ? "$" : "£"}
+                            </span>
+
+                        </div>
                     </div>
             }
-           
+
         </div>
     );
 
