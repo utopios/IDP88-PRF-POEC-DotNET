04a809dfe59e61d547a58ea0f411307b57eef001
utopios <github@utopios.net>
2022-11-30 11:33:10 +0100

    Correction Annuaire

--- 7-React/TP/tp-annuaire-nodejs/src/view/AnnuaireView/AnnuaireView.jsx
+++ 7-React/TP/tp-annuaire-nodejs/src/view/AnnuaireView/AnnuaireView.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import AddContactComponent from '../../component/AddContactComponent/AddContactComponent';
 import ArrayContactComponent from '../../component/ArrayContactComponent/ArrayContactComponent';
 import { getContactApi, postContactApi, updateContactApi, deleteContactApi, postImage } from '../../ContactApiService'
 import './AnnnuaireView.css';
@@ -9,9 +10,15 @@ class AnnuaireView extends Component {
         super(props)
         this.state = {
             contacts: [],
-            //newContact: null,
-            //toggleForm: false
+            newContact: null,
+            toggleForm: false
         }
+    }
+
+    ChangeToggleForm = () => {
+        this.setState({
+            toggleForm: !this.state.toggleForm
+        })
     }
 
     // Methode locale pour récuprer la reponse de l'api et set le state local contact
@@ -20,6 +27,14 @@ class AnnuaireView extends Component {
         this.setState({
             contacts: response.data.data
         })
+    }
+
+    AddContact = async (newContact) => {
+        //console.table(newContact);
+        let response = await postContactApi(newContact);
+        alert(response.data.message);
+        //console.table(response.data.data);
+        this.FetchContact();
     }
 
     EditContact = async (editContact) => {
@@ -43,15 +58,25 @@ class AnnuaireView extends Component {
 
     render() {
         return (
-            <div>
+            <div className='container-fluid'>
                 <h1>Tp Annuaire Node-Js =&gt; React</h1>
 
+                <div className="position-absolute placement">
+                    <AddContactComponent
+                        newContact={this.state.newContact}
+                        AddContact={this.AddContact}
+                        toggleForm={this.state.toggleForm}
+                        ChangeToggleForm={this.ChangeToggleForm}
+                    />
+                </div>
 
-                <ArrayContactComponent
-                    contacts={this.state.contacts}
-                    EditContact={this.EditContact}
-                    DeleteContact={this.DeleteContact}
-                />
+                <div className="contactList">
+                    <ArrayContactComponent
+                        contacts={this.state.contacts}
+                        EditContact={this.EditContact}
+                        DeleteContact={this.DeleteContact}                        
+                    />
+                </div>
             </div>
         );
     }
