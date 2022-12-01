624b1d2057d3a65cc96f006fd0dc13d1640a2bc1
utopios <github@utopios.net>
2022-12-01 14:19:46 +0100

    todolist redux part 2

--- 7-React/Cours/2-Redux/2-store-connect/src/component/TodoComponent/TodoComponent.jsx
+++ 7-React/Cours/2-Redux/2-store-connect/src/component/TodoComponent/TodoComponent.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { toggleTodo } from '../../redux/actions/todosActions';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
 import './TodoComponent.css'
 
-const TodoComponent = () => {
+const TodoComponent = ({ todo, key, toggleTodo }) => {
     return (
-        <div>
-            
-        </div>
+        <React.Fragment key={key}>
+            <li className='todo-item' onClick={() => toggleTodo(todo.id)}>
+            <FontAwesomeIcon
+                icon={faCheck}
+                color={todo.completed ? "green" : "red"}
+            />
+            <span className={todo.completed ? "todo-item-completed" : "todo-item"}>
+                {todo.content}
+            </span>
+        </li>
+        </React.Fragment>
     );
 }
 
-export default TodoComponent;
+//export default TodoComponent;
+export default connect(null, { toggleTodo })(TodoComponent)
--- 7-React/Cours/2-Redux/2-store-connect/src/component/TodoListComponent/TodoListComponent.jsx
+++ 7-React/Cours/2-Redux/2-store-connect/src/component/TodoListComponent/TodoListComponent.jsx
@@ -1,12 +1,30 @@
 import React from 'react';
+import TodoComponent from '../TodoComponent/TodoComponent'
+import { connect } from 'react-redux';
+import { getTodos } from '../../redux/selectors/todoSelector'
 import './TodoListComponent.css';
 
-const TodoListComponent = () => {
+const TodoListComponent = ({ todos }) => {
     return (
-        <div>
-            
+        <div className='container-list'>
+            <ul className='todo-list'>
+                {todos.length ?
+                    todos.map((todo) => {
+                       return <TodoComponent key={todo.id} todo={todo} />
+                    }) : (
+                        <span>Il n'y rien dans la liste!</span>
+                    )
+                }
+
+            </ul>
         </div>
     );
-}
 
-export default TodoListComponent;
+
+}
+const mapStateToProps = state => {
+    const todos = getTodos(state);
+    return { todos };
+}
+export default connect(mapStateToProps)(TodoListComponent)
+//export default TodoListComponent;
