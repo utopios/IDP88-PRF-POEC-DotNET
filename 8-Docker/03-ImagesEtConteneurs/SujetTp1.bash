437009354d8793f5cbde960db654ea6d354dacb6
utopios <github@utopios.net>
2022-12-02 14:03:49 +0100

    Add Tp 1

---
+++ 8-Docker/03-ImagesEtConteneurs/SujetTp1.bash
@@ -0,0 +1,10 @@
+######################################################################
+#				Tp1 - Premier conteneur Debian
+######################################################################
+
+## => Démarer un conteneur à partir d'une image debian:latest
+## => Se connecter au bash du conteneur
+## => mise à jour de ce conteneur
+## => Installer l'editeur nano (vim)
+## => Ecrire un fichier texte contenant du texte nommé text.txt
+## => Afficher le contenu du fichier texte.txt
