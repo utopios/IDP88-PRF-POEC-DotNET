db4aa7bf587b7ea3ba1eda9c6b6ac2c17306429c
utopios <github@utopios.net>
2022-12-05 14:35:05 +0100

    add dockerfile back nodejs

--- 8-Docker/04-Dockerfile/exercices/ApiContact/Instruction.bash
+++ 8-Docker/04-Dockerfile/exercices/ApiContact/Instruction.bash
@@ -2,8 +2,22 @@
 # BACK
 ###
 
-#
-$
+# Build de notre image docker
+$ docker build -t back-api-node .
+
+# Création du container issu de notre image
+$ docker run --name=back-api-container -dp 7777:7777 back-api-node
+
+# Vérification de la présence du conteneur actif
+$ docker ps
+
+# Connection a notre conteneur en bash
+$ docker exec -it back-api-container bash
+
+# Sortir du bash du conteneur
+$ exit
+
+
 
 ###
 # FRONT
