737432ff6d14c6e2afb2e08ea49b5cc9c863be42
utopios <github@utopios.net>
2022-12-15 14:24:32 +0100

    Correction exo 17 à 19 + 14Bis SWITCH

---
+++ 10-DotNet/2-DecouverteCSharp/DecouverteCSharp/Exercices/Exercice14Bis/Program.cs
@@ -0,0 +1,42 @@
+﻿
+Console.WriteLine("\n--- Quelle taille dois-je prendre ? --- \n");
+
+#region Récupération des saisies utilisateur
+Console.Write("Entrez votre taille (en cm) : ");
+int taille = Convert.ToInt32(Console.ReadLine());
+Console.Write("Entrez votre poids (en kg) : ");
+int poids = Convert.ToInt32(Console.ReadLine());
+
+#endregion
+
+#region Structure conditionelle pour affichage des résultats
+
+switch (taille)
+{
+    case int n when n >= 145 && n < 172 && poids >= 43 && poids <= 47 ||
+                    n >= 145 && n < 169 && poids >= 48 && poids <= 53 ||
+                    n >= 145 && n < 166 && poids >= 54 && poids <= 59 ||
+                    n >= 145 && n < 163 && poids >= 60 && poids <= 65:
+        Console.WriteLine("\nPrennez la taille 1.\n");
+        break;
+    case int n when n >= 169 && n < 183 && poids >= 48 && poids <= 53 ||
+                    n >= 166 && n < 178 && poids >= 54 && poids <= 59 ||
+                    n >= 163 && n < 175 && poids >= 60 && poids <= 65 ||
+                    n >= 160 && n < 172 && poids >= 66 && poids <= 71:
+        Console.WriteLine("\nPrennez la taille 2.\n");
+        break;
+    case int n when n >= 178 && n <= 183 && poids >= 54 && poids <= 59 ||
+                    n >= 175 && n <= 183 && poids >= 60 && poids <= 65 ||
+                    n >= 172 && n <= 183 && poids >= 66 && poids <= 71 ||
+                    n >= 163 && n <= 183 && poids >= 72 && poids <= 77:
+        Console.WriteLine("\nPrennez la taille 3.\n");
+        break;
+    default:
+        Console.WriteLine("\nAucune taille ne vous correspond...");
+        break;
+}
+
+#endregion
+
+Console.WriteLine("\nAppuyez sur une touche pour fermer le programme...");
+Console.Read();
---
+++ 10-DotNet/2-DecouverteCSharp/DecouverteCSharp/Exercices/Exercice17/Program.cs
@@ -0,0 +1,41 @@
+﻿
+#region Déclaration variable
+string choix;
+string message;
+#endregion
+
+#region Récupération choix utilisateur
+Console.WriteLine("--- Quelle boisson souhaitez-vous?\n");
+Console.WriteLine("Liste des boissons disponibles :");
+Console.WriteLine("\t1)Eau Plate");
+Console.WriteLine("\t2)Eau Gazeuse");
+Console.WriteLine("\t3)Coca-Cola");
+Console.WriteLine("\t4)Fanta");
+Console.WriteLine("\t5)Sprite");
+Console.WriteLine("\t6)Orangina");
+Console.WriteLine("\t7)7Up");
+Console.Write("Faites votre choix (1 à 7) : ");
+choix = Console.ReadLine();
+#endregion
+
+#region Switch pour tester la saisie utilisateur et afficher le résultat
+Console.WriteLine("");
+switch (choix)
+{
+    case "1": message = "Votre eau plate est servie...\n"; break;
+    case "2": message = "Votre eau gazeuse est servie...\n"; break;
+    case "3": message = "Votre Coca-Cola est servi...\n"; break;
+    case "4": message = "Votre Fanta est servi...\n"; break;
+    case "5": message = "Votre Sprite est servi...\n"; break;
+    case "6": message = "Votre Orangina est servi...\n"; break;
+    case "7": message = "Votre 7Up est servi...\n"; break;
+    default: message = "Mauvais choix !\n"; break;
+}
+#endregion
+
+#region Affichage du résultat
+Console.WriteLine(message);
+#endregion
+
+Console.WriteLine("Appuyez sur enter pour fermer le programme...");
+Console.Read();
