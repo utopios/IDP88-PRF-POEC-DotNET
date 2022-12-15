a50d1a549a05ef02136aa731125c535890c4e676
utopios <github@utopios.net>
2022-12-15 16:45:43 +0100

    Add Cours Boucle while

--- 10-DotNet/2-DecouverteCSharp/DecouverteCSharp/Cours/LesBouclesWhile/Program.cs
+++ 10-DotNet/2-DecouverteCSharp/DecouverteCSharp/Cours/LesBouclesWhile/Program.cs
@@ -1,4 +1,27 @@
-﻿Console.WriteLine("Les Boucles While (tant que...");
+﻿Console.WriteLine("Les Boucles While (tant que...)");
+
+#region La boucle While simple
+int compteur = 1;
+while (compteur <= 50)
+{
+    Console.WriteLine($"Le compteur affiche {compteur}");
+    compteur++;
+}
+#endregion
+
+#region La boucle While simple
+int compteur2 = 1;
+bool valid = false;
+while (!valid)
+{
+    Console.WriteLine($"Le compteur affiche {compteur2}");
+    if (compteur2 == 50)
+    {
+        valid = true;
+    }
+    compteur2++;
+}
+#endregion
 
 Console.Write("Appuyez sur Enter pour fermer le programme...");
 Console.Read();
