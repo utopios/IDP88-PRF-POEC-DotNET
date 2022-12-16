08835fdd15a89b3ece04894d81df35ae743d07e5
utopios <github@utopios.net>
2022-12-16 11:25:34 +0100

    Do...While Cours

---
+++ 10-DotNet/2-DecouverteCSharp/DecouverteCSharp/Cours/LesBouclesDoWhile/Program.cs
@@ -0,0 +1,25 @@
+﻿
+#region Incrémentation d'un compteur
+int compteur = 1;
+do
+{
+    Console.WriteLine($"Le compteur affiche {compteur}");
+    compteur++;
+} while (compteur<=50);
+
+Console.WriteLine($"Une fois sorti de la premiere boucle Do...While, le compteur vaut {compteur}");
+
+do
+{
+    Console.WriteLine($"Le compteur affiche {compteur}");
+    compteur++;
+} while (compteur <= 50);
+
+Console.WriteLine($"Une fois sorti de la deuxieme boucle Do...While, le compteur vaut {compteur}");
+#endregion
+
+
+
+
+Console.WriteLine("Appuyez sur Entrer pour fermer le programme...");
+Console.Read();
