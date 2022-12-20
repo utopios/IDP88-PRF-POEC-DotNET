80234b44ceeeef489c2e9c284cf1d46a99b0792b
utopios <github@utopios.net>
2022-12-20 16:57:01 +0100

    Add Poo Cours

--- 10-DotNet/3-IntroductionPOO/IntroductionPOO/CoursPOO/IntroductionPOO/Classes/Voiture.cs
+++ 10-DotNet/3-IntroductionPOO/IntroductionPOO/CoursPOO/IntroductionPOO/Classes/Voiture.cs
@@ -6,7 +6,49 @@ using System.Threading.Tasks;
 
 namespace IntroductionPOO.Classes
 {
-    internal class Voiture
+    internal class Voiture : object
     {
+        // Attributs (privée)
+        private string model;
+        private string couleur;
+        private int autonomie;
+        private int reservoir;
+
+        public Voiture()
+        {
+
+        }
+
+        public Voiture(string model, string couleur, int autonomie, int reservoir)
+        {
+            Model = model;
+            Couleur = couleur;
+            Autonomie = autonomie;
+            Reservoir = reservoir;
+        }
+
+
+        // Propriètés (Publique)
+        public string Model
+        {
+            get { return model; }
+            set
+            {
+                if (value.Length > 1)
+                    model = value;
+                else
+                    //throw new Exception("Erreur format Model");
+                    Console.WriteLine("Erreur, le nom doit comporter au moins deux caractères...");
+            }
+        }
+        public string Couleur { get => couleur; set => couleur = value; }
+        public int Autonomie { get => autonomie; set => autonomie = value; }
+        public int Reservoir { get => reservoir; set => reservoir = value; }
+
+
+        public override string ToString()
+        {
+            return $"Model : {Model} => Couleur : {Couleur} - Range : {Autonomie} km - Réservoir : {Reservoir}L";
+        }
     }
 }
--- 10-DotNet/3-IntroductionPOO/IntroductionPOO/CoursPOO/IntroductionPOO/Program.cs
+++ 10-DotNet/3-IntroductionPOO/IntroductionPOO/CoursPOO/IntroductionPOO/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using IntroductionPOO.Classes;
+
+static void Afficher(Voiture v)
+{
+    Console.WriteLine($"La voiture est une {v.Model} de couleur {v.Couleur}, avec une autonomie de {v.Autonomie}km avec un réservoir de {v.Reservoir}L.");
+}
+
+// Instanciation d'un objet voiture
+//Voiture voitureDeNicolas = new Voiture();
+Voiture voitureDeNicolas = new();
+// Modifier les props de l'objet voitureDeNicolas
+voitureDeNicolas.Model = "Clio";
+voitureDeNicolas.Couleur = "Noir";
+voitureDeNicolas.Reservoir = 50;
+voitureDeNicolas.Autonomie = 900;
+
+Console.WriteLine(voitureDeNicolas);
+
+Afficher(voitureDeNicolas);
+
+
+Console.WriteLine("Appuyez sur enter pour fermer le programme");
+Console.Read();
