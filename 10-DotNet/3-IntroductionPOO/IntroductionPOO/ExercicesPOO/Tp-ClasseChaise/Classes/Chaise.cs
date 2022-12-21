f17ef3fddcf7f1387529b5c0960cf80e27500fb3
utopios <github@utopios.net>
2022-12-21 12:04:09 +0100

    Add Correction Exo Chaise

---
+++ 10-DotNet/3-IntroductionPOO/IntroductionPOO/ExercicesPOO/Tp-ClasseChaise/Classes/Chaise.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tp_ClasseChaise.Classes
+{
+    internal class Chaise
+    {
+        #region Attibuts
+        private int nbPieds;
+        private string couleur;
+        private string materiaux;
+        #endregion
+
+        #region Constructeur
+        public Chaise() 
+        { 
+        }
+
+        public Chaise(int nbPieds, string couleur, string materiaux)
+        {
+            NbPieds = nbPieds;
+            Couleur = couleur;
+            Materiaux = materiaux;
+        }
+        #endregion
+
+        #region Props
+        public int NbPieds { get => nbPieds; set => nbPieds = value; }
+        public string Couleur { get => couleur; set => couleur = value; }
+        public string Materiaux { get => materiaux; set => materiaux = value; }
+
+        #endregion
+
+        #region Methods
+        public void Afficher()
+        {
+            Console.WriteLine("------------------- Affichage d'un nouvel objet -------------------");
+            Console.WriteLine($"\nJe suis une chaise, avec {nbPieds} pieds en {materiaux} et de couleur {couleur}.\n");
+            Console.WriteLine("-------------------------------------------------------------------");
+        }
+        #endregion
+    }
+}
---
+++ 10-DotNet/3-IntroductionPOO/IntroductionPOO/ExercicesPOO/Tp-ClasseChaise/Program.cs
@@ -0,0 +1,34 @@
+﻿using Tp_ClasseChaise.Classes;
+
+#region Chaise 1
+// Création d'une instance de chaise
+Chaise Chaise1 = new Chaise();
+// Affectation de valeur aux Props
+Chaise1.Couleur = "Bleu";
+Chaise1.Materiaux = "Bois";
+Chaise1.NbPieds = 4;
+// Utilisation de la méthode Afficher()
+Chaise1.Afficher();
+#endregion
+
+#region Chaise 2
+// Création d'une instance de chaise
+Chaise Chaise2 = new Chaise(4,"Blanche", "métal");
+
+// Utilisation de la méthode Afficher()
+Chaise2.Afficher();
+#endregion
+
+#region Chaise 3
+// Création d'une instance de chaise
+Chaise Chaise3 = new Chaise(1,"Transparente","Plexiglass");
+
+// Utilisation de la méthode Afficher()
+Chaise3.Afficher();
+#endregion
+
+
+#region Fin du Programme
+Console.WriteLine("Appuyez sur enter pour fermer le programme");
+Console.Read();
+#endregion
