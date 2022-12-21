cddf270def43b0422607509a2b5fd8e72f9444fb
utopios <github@utopios.net>
2022-12-21 15:13:19 +0100

    Correction TP Salarie

---
+++ 10-DotNet/3-IntroductionPOO/IntroductionPOO/ExercicesPOO/TpClassSalarie/Application.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TpClassSalarie.Classes;
+
+namespace TpClassSalarie
+{
+    internal class Application
+    {
+        public static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            decimal somme = 0;
+
+            #region Salarié N°1
+            Salarie s1 = new Salarie();
+            s1.Nom = "Toto";
+            s1.Categorie = "C001";
+            s1.Matricule = "M001";
+            s1.Service = "S001";
+            s1.Salaire = 2000;
+            #endregion
+
+            #region Salarié N°2
+            Salarie s2 = new Salarie();
+            s2.Nom = "Titi";
+            s2.Categorie = "C002";
+            s2.Matricule = "M002";
+            s2.Service = "S002";
+            s2.Salaire = 2300;
+            #endregion
+
+            #region Salarié N°3
+            Salarie s3 = new Salarie("M003", "C003", "S003", "Tata", 3000);
+            #endregion
+
+            #region Affichage du nombre d'instances
+            Salarie.DisplayCounter();
+            #endregion
+
+            #region Remise à 0 du compteur d'instances
+            Salarie.ResetCounter();
+            #endregion
+
+            #region Salarié N°4
+            Salarie s4 = new Salarie("M004", "C004", "S004", "Tete", 3500);
+            #endregion
+
+            #region Création collection Salarié
+            Salarie[] Salaries = new Salarie[] { s1, s2, s3, s4 };
+            #endregion
+
+            #region Display des Salaires
+            foreach (Salarie s in Salaries)
+            {
+                s.AfficherSalaire();
+                somme += s.Salaire;
+            }
+            #endregion
+
+            #region Affichage des salaires
+            Console.WriteLine("Le montant total des salaires mensuels est de " + somme + "€");
+            Salarie.AfficherSommeSalaire();
+            #endregion
+
+            #region Affichage du nouveau compteur
+
+            Salarie.DisplayCounter();
+
+            #endregion
+
+            #region Appel Volontaire au Garbage Collector (ramasse miettes)
+            GC.Collect();
+            #endregion
+
+
+
+            Console.WriteLine("Appuyez sur Enter pour fermer le programme...");
+            Console.Read();
+        }
+    }
+}
---
+++ 10-DotNet/3-IntroductionPOO/IntroductionPOO/ExercicesPOO/TpClassSalarie/Classes/Salarie.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TpClassSalarie.Classes
+{
+    internal class Salarie
+    {
+        private int id;
+        private string matricule;
+        private string categorie;
+        private string service;
+        private string nom;
+        private decimal salaire;
+        private static int counter = 0;
+        private static decimal totalSalaires = 0;
+
+        public Salarie()
+        {
+            id = ++counter;
+        }
+
+        public Salarie(string name) : this()
+        {
+            Nom = name;
+        }
+        public Salarie(string matricule, string categorie, string service, string nom, decimal salaire) : this(nom)
+        {
+            Matricule = matricule;
+            Categorie = categorie;
+            Service = service;
+            Salaire = salaire;
+            TotalSalaires += salaire;
+        }
+
+        public string Matricule { get => matricule; set => matricule = value; }
+        public string Categorie { get => categorie; set => categorie = value; }
+        public string Service { get => service; set => service = value; }
+        public string Nom { get => nom; set => nom = value; }
+        public decimal Salaire { get => salaire; set => salaire = value; }
+        public int Id { get => id; set => id = value; }
+        public static int Counter { get => counter; set => counter = value; }
+        public static decimal TotalSalaires { get => totalSalaires; set => totalSalaires = value; }
+
+        public virtual void AfficherSalaire()
+        {
+            Console.WriteLine($"Le salaire fixe de {Nom} est de {Salaire}€");
+        }
+
+        public static void AfficherSommeSalaire()
+        {
+            Console.WriteLine($"Le montant total des salaires mensuels est de {TotalSalaires}€");
+        }
+
+        public static void DisplayCounter()
+        {
+            Console.WriteLine($"Le nombre de salarié : {Counter}");
+        }
+        
+        public static void ResetCounter(int value = 0)
+        {
+            Counter = value;
+        }
+
+        ~Salarie()
+        {
+            Console.WriteLine("Destruction...");
+        }
+
+    }
+}
