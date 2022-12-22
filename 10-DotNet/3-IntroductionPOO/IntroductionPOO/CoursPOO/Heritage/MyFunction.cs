0750d7ad0549b8d437c0b291ddb895cec14b4fb0
utopios <github@utopios.net>
2022-12-22 15:06:44 +0100

    Update project

---
+++ 10-DotNet/3-IntroductionPOO/IntroductionPOO/CoursPOO/Heritage/MyFunction.cs
@@ -0,0 +1,55 @@
+﻿using Heritage.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heritage
+{
+    internal class MyFunction
+    {
+        public static void Afficher(Vache v)
+        {
+            Console.WriteLine("Mon nom est {0}", v.NomVache);
+            v.Naissance();
+            v.Reproduction();
+            v.Alimentation();
+            v.Expression();
+            v.Oxigenation();
+            v.Mort();
+        }
+        public static void Afficher(Chien c)
+        {
+            Console.WriteLine("Mon nom est {0}", c.NomChien);
+            c.Naissance();
+            c.Reproduction();
+            c.Alimentation();
+            c.Expression();
+            c.Oxigenation();
+            c.Mort();
+        }
+        public static void Afficher(Fleur f)
+        {
+            Console.WriteLine("Mon nom est {0}", f.NomFleur);
+            f.Naissance();
+            f.Reproduction();
+            f.Alimentation();
+            f.Expression();
+            f.Oxigenation();
+            f.Mort();
+        }
+        public static void Afficher(EtreVivant e)
+        {
+            Console.WriteLine("Mon nom est {0}", e.Nom);
+            Console.WriteLine("Je suis de type {0}", e.Type);
+            e.Naissance();
+            e.Reproduction();
+            e.Alimentation();
+            e.Expression();
+            e.Oxigenation();
+            e.Mort();
+            Console.WriteLine("--------------------------------");
+        }
+    }
+}
