98711caafa3ff27d1a4b35835305bca1811357f6
utopios <github@utopios.net>
2022-12-23 15:05:25 +0100

    Add Tuples cours

---
+++ 10-DotNet/3-IntroductionPOO/IntroductionPOO/CoursPOO/LesTuples/Classes/Tuple.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LesTuples.Classes
+{
+    internal class MyTuple
+    {
+
+        public MyTuple()
+        {
+
+        }
+
+        public (bool, string) GetTuple()
+        {
+            return ( true, "Je suis une chaine");
+        }
+
+        public (bool, string, int) GetTriple()
+        {
+            return (true, "Je suis une chaine", 10);
+        }
+    }
+}
---
+++ 10-DotNet/3-IntroductionPOO/IntroductionPOO/CoursPOO/LesTuples/Program.cs
@@ -0,0 +1,34 @@
+﻿using LesTuples.Classes;
+
+// Les Tuples sont arrivés en C#7
+
+#region Déclaration d'un Tuple
+(double, int) T1 = (4.5, 3);
+
+Console.WriteLine($"Tuple avec en item 1 : {T1.Item1} et en item 2 : {T1.Item2}");
+#endregion
+
+#region Déclaration d'un Tuple as Variable
+(double Sum, int Count) T2 = (4.5, 3);
+
+Console.WriteLine($"Tuple avec en item 1 : {T2.Sum} et en item 2 : {T2.Count}");
+#endregion
+
+
+#region Appel de fonction Tuple
+MyTuple t1 = new();
+
+var result = t1.GetTuple();
+
+Console.WriteLine(result.Item1);
+Console.WriteLine(result.Item2);
+
+var result2 = t1.GetTriple();
+
+Console.WriteLine(result2.Item1);
+Console.WriteLine(result2.Item2);
+Console.WriteLine(result2.Item3);
+#endregion
+
+Console.WriteLine("\nAppuyez sur enter pour fermer le programme");
+Console.Read();
