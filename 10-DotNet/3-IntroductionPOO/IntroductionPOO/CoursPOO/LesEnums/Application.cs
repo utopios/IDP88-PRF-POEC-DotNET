9265485a963f2235c7cf9e9755d060f0e510ae91
utopios <github@utopios.net>
2022-12-23 15:18:31 +0100

    Add Enums cours

---
+++ 10-DotNet/3-IntroductionPOO/IntroductionPOO/CoursPOO/LesEnums/Application.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LesEnums
+{
+    internal class Application
+    {
+        public static void Main(string[] args)
+        {
+            Saison a = Saison.Hiver;
+            Console.WriteLine($"La valeur numérique de {a} est {(int)a}");
+
+            var b = (Saison)1;
+            Console.WriteLine(b);
+            
+            Saison c = (Saison)0;
+            Console.WriteLine(c);
+
+
+
+            Console.WriteLine("\nAppuyez sur enter pour fermer le programme");
+            Console.Read();
+        }
+
+        public enum Saison
+        {
+            Printemps,//0
+            Eté,//1
+            Automne,//2
+            Hiver//3
+        }
+
+    }
+}
