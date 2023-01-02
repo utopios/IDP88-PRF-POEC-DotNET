05e107e523d05740e6193550a9cdf92852855db2
utopios <github@utopios.net>
2023-01-02 10:27:46 +0100

    Update house

--- 10-DotNet/3-IntroductionPOO/IntroductionPOO/ExercicesPOO/MyEnvironement/Classes/House.cs
+++ 10-DotNet/3-IntroductionPOO/IntroductionPOO/ExercicesPOO/MyEnvironement/Classes/House.cs
@@ -32,5 +32,25 @@ namespace MyEnvironement.Classes
         }
 
         public int IdHouse { get => id; set => id = value; }
+
+        public override string ToString()
+        {
+            string chaine = $"Nom Emplacement : {Name} - Type: Maison" +
+                $"\nNombre d'occupants: {Owners.Count}" +
+                $"\nNombre de pièces: {Rooms.Count}\nListe des occupants :";
+            foreach (Person p in Owners)
+            {
+                chaine += p.ToString();
+            }
+            chaine += $"\n{LocationAddress.ToString()}\nListe des pièces :";
+            foreach (Room r in Rooms)
+            {
+                if (Rooms.Count > 0)
+                    chaine += r.ToString();
+                else
+                    chaine += "Aucune pièce, Veuillez ajouter une pièce à la liste";
+            }
+            return chaine;
+        }
     }
 }
