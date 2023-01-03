e79b56c1467e36d77a0975032c5ff74d7c4a1e25
utopios <github@utopios.net>
2023-01-03 14:27:42 +0100

    Les Exceptions

---
+++ 10-DotNet/3-IntroductionPOO/IntroductionPOO/CoursPOO/ExceptionRegex/Class/ExceptionTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionRegex.Class
+{
+    internal class ExceptionTest
+    {
+        public static double safeDivision(double nb1,double nb2)
+        {
+            if (nb2 == 0)
+            {
+                throw new DivideByZeroException("Attention! Vous tentez une division par zéro!");
+            }
+            return nb1 / nb2;
+        }
+    }
+}
--- 10-DotNet/3-IntroductionPOO/IntroductionPOO/CoursPOO/ExceptionRegex/Program.cs
+++ 10-DotNet/3-IntroductionPOO/IntroductionPOO/CoursPOO/ExceptionRegex/Program.cs
@@ -2,6 +2,7 @@
 
 using ExceptionRegex.Class;
 
+#region REGEX
 string maChaine = "Anthony";
 
 Console.WriteLine(Tools.IsName(maChaine));
@@ -15,19 +16,53 @@ Console.WriteLine(Tools.IsPhone(maChaine));
 Client c = new();
 try
 {
-	c.Nom = "Di Persio";
-	c.Prenom = "Anthony";
-	c.Telephone = "+33 6 12 34 56 78";
-	c.Email = "anthony@test.utopios.net";
+    c.Nom = "Di Persio";
+    c.Prenom = "Anthony";
+    c.Telephone = "+33 6 12 34 56 78";
+    c.Email = "anthony@test.utopios.net";
 }
 catch (FormatException e)
 {
-	Console.WriteLine(e.Message);
+    Console.WriteLine(e.Message);
 }
 finally
 {
     Console.WriteLine(c);
 }
+#endregion
+
+
+#region SafeDivision Exception
+Console.WriteLine("----------- SafeDivision -----------");
+Console.Write("Veuillez saisir un premier chffre/nombre => ");
+double nb1 = 0;
+while (!double.TryParse(Console.ReadLine(), out nb1))
+{
+    Console.WriteLine("Erreur de saisie, veuillez saisir un chiffre/nombre...");
+}
+
+double nb2 = 0;
+
+Console.Write("Veuillez saisir un deuxième chffre/nombre => ");
+
+while (!double.TryParse(Console.ReadLine(), out nb2))
+{
+    Console.WriteLine("Erreur de saisie, veuillez saisir une chiffre/nombre...");
+}
+
+double result;
+
+try
+{
+    result=ExceptionTest.safeDivision(nb1,nb2);
+    Console.WriteLine($"{nb1} divisé par {nb2} = {result}");
+}
+catch (DivideByZeroException d)
+{
+    Console.WriteLine(d.Message);
+}
+
+#endregion
 
 
 
