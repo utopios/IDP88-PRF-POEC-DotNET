d91fffe69246671de1158ceeea519ac2209dc81b
utopios <github@utopios.net>
2023-01-04 09:42:15 +0100

    Cours Delegate

---
+++ 10-DotNet/3-IntroductionPOO/IntroductionPOO/CoursPOO/LesDelegues/Class/Calculatrice.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Collections.Specialized.BitVector32;
+
+namespace LesDelegues.Class
+{
+    internal class Calculatrice
+    {
+        #region Ancienne syntaxe
+        //public delegate double DelegateMethodeCalcule(double nb1 , double nb2 );
+
+        //public void Calcule(double a , double b, DelegateMethodeCalcule methode)
+        //{
+        //    Console.WriteLine(methode(a, b));
+        //} 
+
+        //public delegate void DelegateVoid(string s);
+
+        //public void HowToDisplay(string message, DelegateVoid methode)
+        //{
+        //    methode(message);
+        //}
+        #endregion
+
+        #region Action / Func
+        // Deux Type de délégués : Action & Func
+        // ACTION => Délégué sans retour (voir)
+        // FUNC => Délegué avec retour (int, string, bool...etc.)
+
+
+        // Func<double, double,double> Les premiers doubles sont les params et le dernier le type de retour 
+        public void Calcule(double a, double b, Func<double, double, double> methode)
+        {
+            Console.WriteLine(methode(a, b));
+        }
+
+
+        // Action<string> pour les type de paramètres... sans retour
+        public void HowToDisplay(string message, Action<string> methode)
+        {
+            methode(message);
+        }
+        #endregion
+    }
+}
---
+++ 10-DotNet/3-IntroductionPOO/IntroductionPOO/CoursPOO/LesDelegues/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using LesDelegues.Class;
+
+#region Avec un retour
+Calculatrice c = new();
+
+// utilisation d'une fonction locale à la class => Addition
+c.Calcule(10, 20, Addition);
+
+// Utilisation du delegate avec une méthode de soustraction anonyme (expression Lambda)
+c.Calcule(30,40, delegate (double a, double b) { return a - b; });
+
+// Utilisation du delegate avec une méthode de multiplication anonyme (expression Lambda)
+c.Calcule(30, 40, (double a, double b) => { return a * b; });
+
+// Utilisation du delegate avec une méthode de division anonyme (expression Lambda)
+c.Calcule(30, 40, ( a, b) =>  a / b );
+
+#endregion
+
+#region Délégués sans retour
+c.HowToDisplay("Salut à tous!", Afficher);
+#endregion
+
+
+
+double Addition(double a, double b){return a+b;}
+
+void Afficher(string s) { Console.WriteLine(s); }
+void AfficherPlus(string s) { Console.WriteLine(s+"... et j'en affiche plus!"); }
+
+Console.WriteLine("Appuyez sur ENTER pour fermer le programme...");
+Console.Read();
