c37ddd302e002b638fcd324bcfa6bc34015df781
utopios <github@utopios.net>
2023-01-05 16:59:35 +0100

    Sql Cours part 5

---
+++ 10-DotNet/3-IntroductionPOO/IntroductionPOO/1-INNER_JOIN.sql
@@ -0,0 +1,30 @@
+﻿-- INNER JOIN permet de retourner les enregistrment qui ont au moin une ligne dans les DEUX tables
+
+---- Syntaxe
+--SELECT *
+--FROM TableA
+--INNER JOIN TableB
+--ON TableA.Id = TableB.NomColonne
+
+--SELECT *
+--FROM TableA
+--JOIN TableB
+--ON TableA.Id = TableB.NomColonne
+
+
+--Affichage de la table Employee
+SELECT * FROM EMPLOYEE
+
+--Affichage de la table DEPARTMENT
+SELECT * FROM DEPARTMENT
+
+-- INNER JOIN sur les deux tables afin de rassembler les infos des deux tables en une
+SELECT EMP.EMP_ID,
+		Emp.FIRST_NAME,
+		Emp.LAST_NAME,
+		Emp.TITLE,
+		Dep.NAME AS DepartmentName
+FROM EMPLOYEE Emp
+JOIN DEPARTMENT Dep
+ON Emp.DEPT_ID = Dep.DEPT_ID
+ORDER BY Emp.DEPT_ID
---
+++ 10-DotNet/3-IntroductionPOO/IntroductionPOO/2-LEFT_JOIN.sql
@@ -0,0 +1,21 @@
+﻿-- LE mot cle LEFT OUTER JOIN est une jointure externe pour récupérer toutes les infos de la table de gauche
+-- et seulement les lignes ayant une correspondance depuis la deuxieme table vers la premiere.
+
+-- Affichage de la table Customer
+SELECT * FROM CUSTOMER
+
+-- AFFICHAGE DE LA TABLE OFFICER
+SELECT * FROM OFFICER
+
+-- CUSTOMER LEFT OUTER JOIN OFFICER
+-- LEFT OUTTER JOIN = LEFT JOIN
+SELECT Cus.CUST_ID,
+		Cus.STATE,
+		Cus.POSTAL_CODE,
+		Cus.City,
+		Cus.ADDRESS,
+		Ofc.OFFICER_ID,
+		Ofc.FIRST_NAME,Ofc.LAST_NAME,Ofc.TITLE
+FROM CUSTOMER Cus --Table 1
+LEFT OUTER JOIN OFFICER Ofc -- Table 2
+ON Cus.CUST_ID = Ofc.CUST_ID
