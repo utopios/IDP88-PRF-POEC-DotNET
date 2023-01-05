8ff8bf50ae4030dc033930553fc54d9f963a4cee
utopios <github@utopios.net>
2023-01-05 10:31:48 +0100

    SQL PART 1

---
+++ 10-DotNet/3-IntroductionPOO/IntroductionPOO/Req_INSERT.sql
@@ -0,0 +1,11 @@
+﻿INSERT INTO [dbo].[PERSON] ([NOM],[EMAIL],[TELEPHONE])
+VALUES ('Titi','titi@yahoo.fr','+33 6 12 34 56 78')
+
+INSERT INTO dbo.PERSON (NOM,EMAIL,TELEPHONE)
+VALUES ('Toto','toto@yahoo.fr','+33 6 98 76 54 32')
+
+INSERT INTO dbo.PERSON (NOM,EMAIL,TELEPHONE)
+VALUES ('Zorro','zorro@yahoo.fr','+33 6 41 52 63 87'),
+		 ('Jeanne Dark','jeanne@yahoo.fr','+33 6 74 12 58 96'),
+		 ('Delarotule','delarotule@yahoo.fr','+33 6 14 85 96 32')
+		 
