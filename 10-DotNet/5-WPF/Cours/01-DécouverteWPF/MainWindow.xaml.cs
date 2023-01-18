7e504db9ba14161feb77a7ee59d68be681cee631
PetitVulcan <dipersio.a@gmail.com>
2023-01-18 15:10:16 +0100

    Add Part 1 cours WPF

---
+++ 10-DotNet/5-WPF/Cours/01-DécouverteWPF/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using _01_DécouverteWPF.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _01_DécouverteWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //MakeBtn();
+        }
+
+        void MakeBtn()
+        {
+            Button b = new Button()
+            {
+                Content = "Click Me!!!",
+                Foreground = Brushes.Black,
+                Background= Brushes.OrangeRed,
+            };
+            b.Click += Button_Click;
+            grid.Children.Add(b);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Ca marche");
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void MenuItem_Click_Grid(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItem_Click_About(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Ajouter_Click_Btn(object sender, RoutedEventArgs e)
+        {
+            Personne p = null;
+            string nom = NomTbx.Text;
+            string prenom = PrenomTbx.Text;
+            int age = Convert.ToInt32(AgeTbx.Text);
+            if (nom != "" && prenom != "" && age != 0)
+            {
+                p = new(nom, prenom, age);
+            }
+            if (p != null)
+            {
+               ResultatTbc.Text = p.ToString();
+            }
+        }
+    }
+}
---
+++ 10-DotNet/5-WPF/Cours/01-DécouverteWPF/Models/Personne.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_DécouverteWPF.Models
+{
+    class Personne
+    {
+        private string nom;
+        private string prenom;
+        private int age;
+
+        public Personne(string nom, string prenom, int age)
+        {
+            Nom = nom;
+            Prenom = prenom;
+            Age = age;
+        }
+
+        public string Nom { get => nom; set => nom = value; }
+        public string Prenom { get => prenom; set => prenom = value; }
+        public int Age { get => age; set => age = value; }
+
+        public override string ToString()
+        {
+            return $"Nom : {Nom} - Prénom : {Prenom} -  Age : {Age}";
+        }
+    }
+}
