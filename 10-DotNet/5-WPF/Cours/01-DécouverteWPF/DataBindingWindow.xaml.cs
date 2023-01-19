f1767d926fd5177afdfb1fa6ad3a1287e950e379
PetitVulcan <dipersio.a@gmail.com>
2023-01-19 15:08:13 +0100

    DataBindingWPF

---
+++ 10-DotNet/5-WPF/Cours/01-DécouverteWPF/DataBindingWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace _01_DécouverteWPF
+{
+    /// <summary>
+    /// Logique d'interaction pour DataBindingWindow.xaml
+    /// </summary>
+    public partial class DataBindingWindow : Window
+    {
+        public DataBindingWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
