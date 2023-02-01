3c415d96309d0236dba5e74e017d21a7c2df94ff
utopios <github@utopios.net>
2023-02-01 15:29:03 +0100

    correction exercice 1

---
+++ 10-DotNet/AspNetCore/CoursAspNetCore/CoursAspNetCore/Controllers/ContactController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoursAspNetCore.Controllers
+{
+    public class ContactController : Controller
+    {
+       public string Index()
+        {
+            return "Je suis la page liste contacts";
+        }
+
+        public string detail()
+        {
+            return "Je suis la page detail contact";
+        }
+
+        public string form()
+        {
+            return "Je suis la page pour ajouter un contact";
+        }
+    }
+}
