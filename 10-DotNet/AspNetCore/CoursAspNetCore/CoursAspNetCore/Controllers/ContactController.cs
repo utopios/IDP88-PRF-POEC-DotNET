3b5f45cc6d1eb71d3fa1be7c699939646d68167a
utopios <github@utopios.net>
2023-02-02 11:39:07 +0100

    Utilisation des viewbag et viewdata

--- 10-DotNet/AspNetCore/CoursAspNetCore/CoursAspNetCore/Controllers/ContactController.cs
+++ 10-DotNet/AspNetCore/CoursAspNetCore/CoursAspNetCore/Controllers/ContactController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CoursAspNetCore.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CoursAspNetCore.Controllers
 {
@@ -7,19 +8,29 @@ namespace CoursAspNetCore.Controllers
        public IActionResult Index()
         {
             //return "Je suis la page liste contacts";
+            List<Contact> contacts = new List<Contact>() { 
+                new Contact() { Name = "toto" },
+                new Contact() { Name = "titi" },
+                new Contact() { Name = "tata" },
+            };
+
+            //Pour passer des données à la vue on peut utiliser le viewData
+            //ViewData["contacts"] = contacts;
+            //Pour passer des données à la vue on peut utiliser le ViewBag
+            ViewBag.Contacts = contacts;
             return View();
         }
 
         public IActionResult detail(int? id)
         {
             //return "Je suis la page detail contact "+id;
-            return View();
+            return View("Index");
         }
 
         public IActionResult form()
         {
             //return "Je suis la page pour ajouter un contact";
-            return View();
+            return View("~/Views/Home/Index.cshtml");
         }
 
         public string CustomDetail(string firstName, string lastName) { 
---
+++ 10-DotNet/AspNetCore/CoursAspNetCore/CoursAspNetCore/Models/Contact.cs
@@ -0,0 +1,10 @@
+﻿namespace CoursAspNetCore.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+    }
+}
