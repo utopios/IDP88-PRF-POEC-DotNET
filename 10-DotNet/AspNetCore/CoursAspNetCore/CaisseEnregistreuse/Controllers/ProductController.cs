ec7a542286c6d462218abb7330e8c2ab6b4e8cf5
utopios <github@utopios.net>
2023-02-02 11:08:03 +0100

    correction exercice 3

---
+++ 10-DotNet/AspNetCore/CoursAspNetCore/CaisseEnregistreuse/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CaisseEnregistreuse.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Detail(int? id)
+        {
+            return View();
+        }
+
+        public IActionResult Form()
+        {
+            return View();
+        }
+    }
+}
---
+++ 10-DotNet/AspNetCore/CoursAspNetCore/CaisseEnregistreuse/Controllers/SaleController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CaisseEnregistreuse.Controllers
+{
+    public class SaleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
