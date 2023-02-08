9d5e2248090406a3ce2fc21ba0f23e9914338c07
utopios <github@utopios.net>
2023-02-08 14:26:32 +0100

    TDD fizbuzz cours

---
+++ 10-DotNet/CoursTDD/CoursTDD/CoursTDD/FizBuzz.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoursTDD
+{
+    public class FizBuzz
+    {
+        private int number;
+
+        public FizBuzz(int number)
+        {
+            this.number = number;
+        }
+
+        public string Run()
+        {
+            string result = "";
+            if(number % 3 == 0 && number%5 == 0)
+            {
+                result = "fizbuzz";
+            } 
+            else if(number % 3 == 0)
+            {
+                result = "fiz";
+            }
+            else if(number % 5 == 0)
+            {
+                result = "buzz";
+            }
+            else
+            {
+                result = number.ToString();
+            }
+            return result;
+        }
+    }
+}
---
+++ 10-DotNet/CoursTDD/CoursTDD/CoursTest/FizBuzzTest.cs
@@ -0,0 +1,54 @@
+﻿using CoursTDD;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoursTest
+{
+    [TestClass]
+    public class FizBuzzTest
+    {
+        private FizBuzz fizBuzz;
+
+        [TestMethod]
+        public void TestFizBuzzWhenNumer_3_Then_Fiz()
+        {
+            fizBuzz = new FizBuzz(3);
+
+            string result = fizBuzz.Run();
+
+            Assert.AreEqual("fiz", result);
+        }
+
+        [TestMethod]
+        public void TestFizBuzzWhenNumer_5_Then_Buzz()
+        {
+            fizBuzz = new FizBuzz(10);
+
+            string result = fizBuzz.Run();
+
+            Assert.AreEqual("buzz", result);
+        }
+        [TestMethod]
+        public void TestFizBuzzWhenNumer_5_3_Then_FizBuzz()
+        {
+            fizBuzz = new FizBuzz(15);
+
+            string result = fizBuzz.Run();
+
+            Assert.AreEqual("fizbuzz", result);
+        }
+
+        [TestMethod]
+        public void TestFizBuzzWhenNumer_Other_Then_number()
+        {
+            fizBuzz = new FizBuzz(26);
+
+            string result = fizBuzz.Run();
+
+            Assert.AreEqual("26", result);
+        }
+    }
+}
