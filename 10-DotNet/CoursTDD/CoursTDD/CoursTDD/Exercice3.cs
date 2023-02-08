8ad262a8df67696c73524c2cbfe976308ef0e2bc
utopios <github@utopios.net>
2023-02-08 15:47:42 +0100

    correction exercice 3 tdd

---
+++ 10-DotNet/CoursTDD/CoursTDD/CoursTDD/Exercice3.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoursTDD
+{
+    public class Exercice3
+    {
+        public bool IsLeap(int year)
+        {
+            return (year % 400 == 0 && year % 4000 != 0) || (year % 4 == 0 && year % 100 != 0);
+        }
+    }
+}
---
+++ 10-DotNet/CoursTDD/CoursTDD/CoursTest/Exercice3Test.cs
@@ -0,0 +1,42 @@
+﻿using CoursTDD;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoursTest
+{
+    [TestClass]
+    public class Exercice3Test
+    {
+        private Exercice3 ex = new Exercice3();
+        [TestMethod]
+        public void TestLeap_400_Should_True()
+        {
+            bool res = ex.IsLeap(1200);
+            Assert.IsTrue(res);
+        }
+
+        [TestMethod]
+        public void TestLeap_4_Not_100_Should_True()
+        {
+            bool res = ex.IsLeap(12);
+            Assert.IsTrue(res);
+        }
+
+        [TestMethod]
+        public void TestLeap_4000_Should_False()
+        {
+            bool res = ex.IsLeap(8000);
+            Assert.IsFalse(res);
+        }
+
+        [TestMethod]
+        public void TestLeap_1999_Should_False()
+        {
+            bool res = ex.IsLeap(1999);
+            Assert.IsFalse(res);
+        }
+    }
+}
