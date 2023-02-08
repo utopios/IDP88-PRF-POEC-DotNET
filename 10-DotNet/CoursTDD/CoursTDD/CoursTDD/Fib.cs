9da3ade31dc292a318b3fce89843091e162537ef
utopios <github@utopios.net>
2023-02-08 14:01:50 +0100

    correction exercice test

---
+++ 10-DotNet/CoursTDD/CoursTDD/CoursTDD/Fib.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoursTDD
+{
+    public class Fib
+    {
+
+        private int range;
+
+        public Fib(int r)
+        {
+            range = r;
+        }
+
+        public List<int> GetFibSeries()
+        {
+            List<int> result = new List<int>();
+            int a = 0, b = 1, c = 0;
+            if (range == 1)
+            {
+                result.Add(0);
+                return result;
+            }
+            result.Add(0);
+            result.Add(1);
+            for (int i = 2; i < range; i++)
+            {
+                c = a + b;
+                result.Add(c);
+                a = b;
+                b = c;
+            }
+            return result;
+        }
+
+        
+    }
+}
---
+++ 10-DotNet/CoursTDD/CoursTDD/CoursTest/FibTest.cs
@@ -0,0 +1,87 @@
+﻿using CoursTDD;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoursTest
+{
+    [TestClass]
+    public class FibTest
+    {
+        private Fib fib;
+
+        private void SetUp(int r)
+        {
+            fib = new Fib(r);
+        }
+
+        [TestMethod]
+        public void GetFibSeriesTestWhen_1_ThenNotEmpty()
+        {
+            SetUp(1);
+            //Act
+            List<int> result = fib.GetFibSeries();
+            Assert.AreEqual(1, result.Count);
+        }
+        [TestMethod]
+        public void GetFibSeriesTestWhen_1_ThenResultContains_0()
+        {
+            SetUp(1);
+            List<int> expected = new() { 0 };
+            //Act
+            List<int> result = fib.GetFibSeries();
+            
+            //Assert.ReferenceEquals(expected, result) ;
+            CollectionAssert.AreEqual(expected, result);
+        }
+        [TestMethod]
+        public void GetFibSeriesTestWhen_Range_6_ThenResultContains_3()
+        {
+            SetUp(6);
+            
+            //Act
+            List<int> result = fib.GetFibSeries();
+
+            Assert.IsTrue(result.Contains(3)) ;
+            
+        }
+
+        [TestMethod]
+        public void GetFibSeriesTestWhen_Range_6_ThenResultCount_6()
+        {
+            SetUp(6);
+
+            //Act
+            List<int> result = fib.GetFibSeries();
+
+            Assert.AreEqual(6, result.Count);
+
+        }
+        [TestMethod]
+        public void GetFibSeriesTestWhen_Range_6_ThenResultDontContains_4()
+        {
+            SetUp(6);
+
+            //Act
+            List<int> result = fib.GetFibSeries();
+
+            Assert.IsFalse(result.Contains(4));
+
+        }
+
+        [TestMethod]
+        public void GetFibSeriesTestWhen_Range_6_ThenResultIsSorted()
+        {
+            SetUp(6);
+            List<int> expected = new() { 0, 1, 1, 2, 3, 5 };
+            //Act
+            List<int> result = fib.GetFibSeries();
+
+            CollectionAssert.AreEqual(expected, result);
+
+        }
+
+    }
+}
