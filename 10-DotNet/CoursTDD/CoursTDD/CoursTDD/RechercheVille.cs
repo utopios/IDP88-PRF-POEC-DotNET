70443c94bb7de9599a1f1f9b9c7e5b189bbacc23
utopios <github@utopios.net>
2023-02-09 09:34:58 +0100

    correction exercice 4

---
+++ 10-DotNet/CoursTDD/CoursTDD/CoursTDD/RechercheVille.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoursTDD
+{
+    public class RechercheVille
+    {
+        private List<string> villes;
+
+        public List<string> Villes { get => villes; set => villes = value; }
+
+        public RechercheVille() {         
+            Villes= new List<string>();
+        }
+
+        public List<string> Rechercher(string mot)
+        {
+            
+            if(mot.Length < 2)
+            {
+                if (mot == "*")
+                {
+                    return Villes;
+                }
+                throw new NotFoundException();
+            }
+            else
+            {
+                return Villes.Where(x => x.ToLower().Contains(mot.ToLower())).ToList();
+            }
+        }
+    }
+}
---
+++ 10-DotNet/CoursTDD/CoursTDD/CoursTest/RechercheVilleTest.cs
@@ -0,0 +1,56 @@
+﻿using CoursTDD;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoursTest
+{
+    [TestClass]
+    public class RechercheVilleTest
+    {
+        private RechercheVille rechercheVille = new RechercheVille();
+        [TestMethod]
+        public void RechercheTestWhenLess_2_Char_Then_Raise_NotFoundException()
+        {
+            Assert.ThrowsException<NotFoundException>(() => rechercheVille.Rechercher("a"));
+        }
+
+        [TestMethod]
+        public void RechercheTestWhenGt_2_Char_Then_Return_Cities_Started_With_Char()
+        {
+            rechercheVille.Villes = new List<string> { "Valence", "Vancouver", "Amsterdam", "Vienne", "Sydney", "New York" };
+            List<string> result = rechercheVille.Rechercher("Va");
+
+            CollectionAssert.AreEqual(new List<string>() { "Valence", "Vancouver" }, result);
+        }
+
+        [TestMethod]
+        public void RechercheTest_No_Case_Sensitive()
+        {
+            rechercheVille.Villes = new List<string> { "Valence", "Vancouver", "Amsterdam", "Vienne", "Sydney", "New York" };
+            List<string> result = rechercheVille.Rechercher("vA");
+
+            CollectionAssert.AreEqual(new List<string>() { "Valence", "Vancouver" }, result);
+        }
+
+        [TestMethod]
+        public void RechercheTestWhenGt_2_Char_Then_Return_Cities_Contains_With_Char()
+        {
+            rechercheVille.Villes = new List<string> { "Valence", "Vancouver", "Amsterdam", "Vienne", "Sydney", "New York" };
+            List<string> result = rechercheVille.Rechercher("Er");
+
+            CollectionAssert.AreEquivalent(new List<string>() { "Amsterdam", "Vancouver" }, result);
+        }
+
+        [TestMethod]
+        public void RechercheTestWhen_Char_Is_Asterisk_Then_Return_All_Cities()
+        {
+            rechercheVille.Villes = new List<string> { "Valence", "Vancouver", "Amsterdam", "Vienne", "Sydney", "New York" };
+            List<string> result = rechercheVille.Rechercher("*");
+
+            CollectionAssert.AreEquivalent(new List<string> { "Valence", "Vancouver", "Amsterdam", "Vienne", "Sydney", "New York" }, result);
+        }
+    }
+}
