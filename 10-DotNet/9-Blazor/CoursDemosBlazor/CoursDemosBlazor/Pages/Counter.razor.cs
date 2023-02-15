d3bca5aa55a93be5a51db9d5512b824507dbd708
utopios <github@utopios.net>
2023-02-15 12:32:04 +0100

    blazor parties 1-2-3

---
+++ 10-DotNet/9-Blazor/CoursDemosBlazor/CoursDemosBlazor/Pages/Counter.razor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace CoursDemosBlazor.Pages
+{
+    public partial class Counter
+    {
+        [Parameter]
+        public int Value { get; set; }
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+        private int currentCount = 0;
+
+        void IncrementCount()
+        {
+            currentCount++;
+            if (currentCount == 13)
+                NavigationManager.NavigateTo("counter2/13");
+
+        }
+    }
+}
