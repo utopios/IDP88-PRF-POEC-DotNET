6dfac46936dbef596b5e7dcd753a782abcde6d95
utopios <github@utopios.net>
2023-02-16 10:16:41 +0100

    demo form

---
+++ 10-DotNet/9-Blazor/CoursDemosBlazor/CoursDemosBlazor/Models/Marmoset.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoursDemosBlazor.Models
+{
+    public class Marmoset
+    {
+        public int Id { get; set; }
+        [Required, StringLength(12, MinimumLength = 3, ErrorMessage = "Le nom doit faire entre 3 et 12 caractères")]
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        [Required]
+        public bool OwnsBanana { get; set; }
+        [Required]
+        public FurColors FurColor { get; set; }
+        public enum FurColors
+        {
+            Red,
+            Brown,
+            Yellow,
+            Orange,
+            White
+        }
+
+        public override string? ToString()
+        {
+            return $"{Name} {Description} {OwnsBanana} {FurColor}";
+        }
+    }
+}
